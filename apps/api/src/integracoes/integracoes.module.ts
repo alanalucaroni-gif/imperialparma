149082046a2104335d0e53cdb3acd10c95cfa1e4
alanalucaroni-gif <alanalucaroni@gmail.com>
2026-07-30 11:57:03 -0300

    conecta confirmação do iFood ao estoque e à logística

--- apps/api/src/integracoes/integracoes.module.ts
+++ apps/api/src/integracoes/integracoes.module.ts
@@ -1,7 +1,14 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "../auth/auth.module.js";
+import { IfoodPedidosService } from "./ifood-pedidos.service.js";
+import { IfoodWebhookController } from "./ifood-webhook.controller.js";
 import { IntegracoesController, WhatsappMetaController } from "./integracoes.controller.js";
 import { IntegracoesService } from "./integracoes.service.js";
 
-@Module({ imports: [AuthModule], controllers: [IntegracoesController, WhatsappMetaController], providers: [IntegracoesService], exports: [IntegracoesService] })
+@Module({
+  imports: [AuthModule],
+  controllers: [IntegracoesController, WhatsappMetaController, IfoodWebhookController],
+  providers: [IntegracoesService, IfoodPedidosService],
+  exports: [IntegracoesService, IfoodPedidosService],
+})
 export class IntegracoesModule {}
